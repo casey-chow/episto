fbd8f2d0af3970ce8c4c0a19b8170fdb28d32171
Casey Chow <casey.chow@gmail.com>
2014-08-13 00:26:40 -0400

    reimplement recording demo in interface

--- api/controllers/RecordingController.js
+++ api/controllers/RecordingController.js
@@ -14,6 +14,71 @@
  *
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
+var async = require('async');
+var fs = require('fs-extra');
+var path = require('path');
+var execFile = require('child_process').execFile;
+
+var my = {
+  /**
+   * Combines the WAV chunks into a single sample.
+   *
+   * @param {Array} chunks - an array of the base64-encoded WAV chunks
+   * @param {Function} cb - a callback with the signature cb(compilation), 
+   *                        which receives the final base64-encoded compilation
+   */
+  combineAudioChunks: function(chunks, cb) {
+    var length = chunks.length;
+
+    async.seq(
+      function createDirectory(done) {
+        fs.mkdirp(path.resolve('.tmp/audio'), done);
+      },
+      function bufferizeChunks(err, done) {
+        if (err) { sails.log.error(err); }
+        var files = this.files = [];
+
+        chunks = _.map(chunks, function bufferizeChunk(chunk, index) {
+          /** 
+           * Assumption here is that the chunk starts with 
+           * `data:audio/wav;base64,`, which is 22 chars long.
+           */
+          chunk = chunk.slice(22); 
+          return [new Buffer(chunk, 'base64'), index];
+        });
+
+        async.each(chunks, function writeBuffer(arr, _done) {
+          var chunkBuffer = arr[0];
+          var index = arr[1];
+          var fileName = path.resolve('.tmp/audio/'+index+'.wav');
+
+          files.push(fileName);
+          sails.log('Writing to file ' + fileName);
+          fs.writeFile(fileName, chunkBuffer, _done);
+        }, done);
+      },
+      function soxCombine(done) {
+        sails.log('Combining with sox');
+        this.files.push(path.resolve('.tmp/audio/out.wav'));
+        sails.log.verbose('Calling sox with parameters: ' + this.files.join(' '));
+        execFile('sox', this.files, done);
+      },
+      function readFile(stdout, stderr, done) {
+        if (stdout) { sails.log(stdout); }
+        if (stderr) { sails.log.error(stderr); }
+
+        sails.log.info('Reading output file');
+        fs.readFile(path.resolve('.tmp/audio/out.wav'), done);
+      },
+      function returnBase64(finalBuffer, done) {
+        cb('data:audio/wav;base64,' + finalBuffer.toString('base64'));
+        fs.remove(path.resolve('.tmp/audio/'), done);
+      }
+    )(function() {
+      sails.log('[Chunks] Completed Task');
+    });
+  }
+}
 
 module.exports = {
     
@@ -28,10 +93,53 @@ module.exports = {
   },
 
   index: function(req, res) {
-    sails.log('RecordingController.index');
-    sails.log.info(req.params);
+    sails.log.verbose('RecordingController.index');
     res.json(req.params);
+  },
+
+  chunk: function(req, res) {
+    var chunks = req.session.chunks = req.session.chunks || [];
+
+    if (req.method == 'POST' && req.param('done')) {
+      sails.log.verbose('[Chunks] Stream ended.');
+      res.json({
+        success: true,
+        chunks: chunks.length
+      });
+
+      return;
+    }
+
+    var audioData = req.param('audio');
+    var audioExcerpt = audioData.slice(-128);
+
+    sails.log.verbose('[Chunks] Received Audio: ' + audioExcerpt);
+    chunks.push(audioData);
+    sails.log.verbose('[Chunks] Chunk collection now at length ' + chunks.length);
+
+    res.json({
+      success: true,
+      message: 'Chunk successfully stored.',
+      length: audioData.length,
+      chunks: chunks.length
+    });
+
+  },
+
+  compile: function(req, res) {
+    var chunks = req.session.chunks = req.session.chunks || [];
+
+    sails.log.verbose('[Chunks] Compiling chunks');
+
+    my.combineAudioChunks(chunks, function(compilation) {
+      sails.log.info('[Chunks] Sending chunk result: ' + compilation.slice(-128));
+      res.send({
+        success: true,
+        message: compilation,
+        length: chunks.reduce(function(sum, cur) { return sum + cur.length; }, 0)
+      });
+    });
+
   }
 
-  
 };
---
+++ assets/js/recorder.js
@@ -0,0 +1,242 @@
+/**
+ * recorder.js
+ *
+ * @overview 
+ * Given an audio stream, this demo will record the streams in 2-sceond segments
+ * using the webaudio API and pipe them over socket.io.
+ *
+ *= require jquery
+ *= require lodash
+ *= require socket.io
+ *= require sails.io
+ *= require q
+ *= require wavencoder
+ */
+
+window.Recorder = (function recorder() {
+
+
+  var my = {
+
+    $startRecording: $('#recording-start'),
+    $stopRecording: $('#recording-stop'),
+    $status: $('#status'),
+    $audioElement: $('#player'),
+
+    log: console.log.bind(console),
+    error: console.error.bind(console),
+    time: console.time.bind(console),
+    timeEnd: console.timeEnd.bind(console),
+
+    status: function(msg) { 
+      my.$status.fadeOut(function() { $(this).html(msg); }).fadeIn();
+    },
+
+    /** 
+     * Get the user media stream as a promise.
+     * @returns {Promise} The promise of a stream object.
+     */
+    getUserMedia: function() {
+      var promise = my.userMedia = 
+      my.userMedia || Q.Promise(function(resolve, reject, notify) {
+        navigator.webkitGetUserMedia({ audio: true }, resolve, reject);
+      });
+
+      return promise;
+    },
+
+    get: function(url) {
+      return Q.Promise(function(resolve, reject, notify) {
+        socket.get(url, resolve);
+      });
+    },
+    send: function(url, msg) {
+      return Q.Promise(function(resolve, reject) {
+        socket.post(url, msg, resolve);
+      });
+    }
+  };
+
+  var Recorder = Stapes.subclass({
+
+    /** @constructor */
+    constructor: function() {
+      my.log("[Episto] loading code");
+
+      my.bufferSize = 4096;
+
+      my.wavEncoder = new WavEncoder(my.bufferSize, {
+        sampleRateHz: 48000
+      });
+
+
+      this.bindEvents();
+    },
+
+    /** Bind all the events needed for the application. */
+    bindEvents: function() {
+
+      my.$startRecording.click(this.onStartRecording.bind(this));
+      my.$stopRecording.click(this.onStopRecording.bind(this));
+
+      this.on({
+        'connect': function() {
+          my.status('Ready to Record');
+          my.$startRecording.prop('disabled', false);
+        },
+
+        'recording:start': function() {
+          my.status('Recording');
+          my.time("[Episto] Recording Audio");
+
+
+          my.$startRecording.prop('disabled', true);
+          my.$stopRecording.prop('disabled', false);
+        },
+
+        'processing:chunk': function(chunkBuffer) {
+          my.log('[Episto] New chunk issued with length: ' 
+            + chunkBuffer.length);
+        },
+        'upload:chunk:start': function(audioDataURL) {
+          my.log('[Episto] Uploading Chunk:', audioDataURL);
+          my.time('[Episto] Sending Chunk to Server');
+        },
+        'upload:chunk:complete': function(res) {
+          my.timeEnd('[Episto] Sending Chunk to Server');
+          my.log('[Episto] Server Returned', res)
+        },
+
+        'recording:stop': function() {
+          my.status('Recording Finished');
+          my.timeEnd("[Episto] Recording Audio");
+          my.$stopRecording.addClass('pure-button-disabled');
+
+          my.$startRecording.prop('disabled', false);
+          my.$stopRecording.prop('disabled', true);
+
+        },
+
+        'compilation:start': function() {
+          my.status('Requesting Compiled Audio');
+
+          my.time('[Episto] Requesting Compilation');
+          my.log('[Episto] Requesting Compilation');
+        },
+
+        'compilation:complete': function(res) {
+          my.status('Compiled Audio Retrieved, Ready to Record');
+
+          my.timeEnd('[Episto] Requesting Compilation');
+          my.log('[Episto] Server Response:', res);
+
+          my.$startRecording.prop('disabled', false);
+          my.$stopRecording.prop('disabled', true);
+        }
+      });
+    },
+
+    /** Callback binding for the ending of the recording. */
+    onStartRecording: function() {
+      var that = this;
+
+      my.getUserMedia().then(function(audioStream) {
+
+        // Set up the WebAudio API
+        my.context = new webkitAudioContext();
+        my.audioSource = my.context.createMediaStreamSource(audioStream);
+        my.scriptProcessor = my.context.createScriptProcessor(my.bufferSize,1,1);
+
+        my.scriptProcessor.onaudioprocess = that.onChunk.bind(that);
+
+        // Plug everything in
+        my.audioSource.connect(my.scriptProcessor);
+        my.scriptProcessor.connect(my.context.destination);
+
+      }).catch(function(e) {
+        my.error(e);
+      }).then(function() {
+        that.emit('recording:start');
+      }).done();
+
+      return false;
+
+    },
+
+    onChunk: function(e) {
+      var that = this;
+
+      var inBuffer = e.inputBuffer.getChannelData(0);
+      var outBuffer = e.outputBuffer.getChannelData(0);
+
+      // Only for if you care about getting the audio back to the browser.
+      // Which we don't.
+      // outBuffer.set(inBuffer); 
+      
+      this.emit('processing:chunk', inBuffer);
+
+      /** Encode the data into a base64-enoded WAV file. 
+       * @see https://github.com/fritzo/wavencoderjs
+       */
+      var chunk = my.wavEncoder.encode(inBuffer);
+
+      this.emit('upload:chunk:start', chunk);
+
+      // Upload das audio through socket
+      my.send('/recordings/chunk', { 
+        audio: chunk,
+        done: false,
+        type: 'audio/wav'
+      }).then(function(res) {
+        that.emit('upload:chunk:complete', res);
+      }).done();
+
+    },
+
+    /** Callback binding for the ending of the recording. */
+    onStopRecording: function() {
+      var self = this;
+      
+      my.getUserMedia().then(function(audioStream) {
+
+        audioStream.stop();
+
+        my.scriptProcessor.onaudioprocess = null;
+        my.audioSource.disconnect();
+        my.scriptProcessor.disconnect();
+
+        return Q.Promise(function(resolve, reject, notify) {
+          socket.post('/recordings/chunk', {
+            done: true
+          }, resolve);
+        });
+
+      }).then(function() {
+
+        self.emit('recording:stop upload:complete');
+
+        self.requestAudio();
+
+      }).done();
+
+      return false;
+    },
+
+    /** Request the compiled audio from the server. */
+    requestAudio: function() {
+      var that = this;
+
+      this.emit('compilation:start');
+
+      my.get('/recordings/compile').then(function(res) {
+        that.emit('compilation:complete', res);
+
+        my.$audioElement.attr('src', res.message);
+      }).done();
+
+    }
+  });
+
+  return new Recorder();
+
+}());
